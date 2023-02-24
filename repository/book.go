9cc9912d588f94fe87e93dce118477a7a7bacc03
Bohdan <bohdanfugas@gmail.com>
2023-02-25 01:01:13 +0400

    updated MarshalJSON and UnmarshalJSON methods to fit their interfaces

--- repository/book.go
+++ repository/book.go
@@ -10,8 +10,8 @@ import (
 type BooksRepo interface {
 	GetBooks() (entities.Books, error)
 	SetBooks(newBooks entities.Books) error
-	UnmarshalJson() error
-	MarshalJson(newBooks entities.Books) ([]byte, error)
+	UnmarshalJSON(byteValue []byte) error
+	MarshalJSON() ([]byte, error)
 }
 
 type booksRepo struct {
@@ -27,8 +27,15 @@ func CreateNewBook() entities.Book {
 }
 
 func (b *booksRepo) GetBooks() (entities.Books, error) {
-	err := b.UnmarshalJson()
+	jsonFile, err := os.Open("database/books.json")
+	defer jsonFile.Close()
+	if err != nil {
+		return nil, err
+	}
 
+	byteValue, _ := io.ReadAll(jsonFile)
+
+	err = b.UnmarshalJSON(byteValue)
 	if err != nil {
 		return nil, err
 	}
@@ -37,7 +44,8 @@ func (b *booksRepo) GetBooks() (entities.Books, error) {
 }
 
 func (b *booksRepo) SetBooks(newBooks entities.Books) error {
-	byteArr, err := b.MarshalJson(newBooks)
+	b.books = newBooks
+	byteArr, err := b.MarshalJSON()
 
 	// update our JSON with users
 	err = os.WriteFile("database/books.json", byteArr, 0666)
@@ -48,16 +56,8 @@ func (b *booksRepo) SetBooks(newBooks entities.Books) error {
 	return nil
 }
 
-func (b *booksRepo) UnmarshalJson() error {
-	jsonFile, err := os.Open("database/books.json")
-	defer jsonFile.Close()
-
-	if err != nil {
-		return err
-	}
-	byteValue, _ := io.ReadAll(jsonFile)
-
-	err = json.Unmarshal(byteValue, &b.books)
+func (b *booksRepo) UnmarshalJSON(byteValue []byte) error {
+	err := json.Unmarshal(byteValue, &b.books)
 	if err != nil {
 		return err
 	}
@@ -65,9 +65,8 @@ func (b *booksRepo) UnmarshalJson() error {
 	return nil
 }
 
-func (b *booksRepo) MarshalJson(newBooks entities.Books) ([]byte, error) {
-	byteArr, err := json.MarshalIndent(&newBooks, "", "\t")
-
+func (b *booksRepo) MarshalJSON() ([]byte, error) {
+	byteArr, err := json.MarshalIndent(&b.books, "", "\t")
 	if err != nil {
 		return nil, err
 	}
